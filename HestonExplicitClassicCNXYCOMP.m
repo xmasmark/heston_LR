9d206bdb9ee305a08fa905b19489464e1c039b5e
Marco Abramo <marco.abramo@escaladetek.com>
2024-11-28 16:49:52 +0100

    mmmm

--- HestonExplicitClassicCNXYCOMP.m
+++ HestonExplicitClassicCNXYCOMP.m
@@ -100,6 +100,98 @@ function U = HestonExplicitClassicCNXYCOMP(params,K,r,q,S,V,T)
 
         %here is the vectorised part
 
+        boundaryMatrix_S = zeros(NS, NV);
+        boundaryMatrix_S(NS,:) = (1/ds);  %Neumann condition at large S
+        bm_S=boundaryMatrix_S;
+
+        boundaryMatrix_S2 = zeros(NS, NV);
+        boundaryMatrix_S2(NS,:) = (1/ds);  %Neumann condition at large S
+        bm_S2=boundaryMatrix_S2;
+
+        boundaryMatrix_V = zeros(NS, NV);
+        boundaryMatrixT = boundaryMatrix_V';  % Transpose for volatility derivatives
+        boundaryMatrixT(NV,:) = S;  % Boundary at max volatility
+
+        % Boundary Condition when V = 0 (volatility = 0)
+        discountedPayoff = max((S - K * exp(-r * (Tmax - T(t)))), 0);
+        %discountedPayoff = max((S - K), 0);
+        boundaryMatrixT(1,:) = discountedPayoff;
+
+        bm_V1 = boundaryMatrixT;
+
+        boundaryMatrix_mixed = zeros(NS, NV);
+        boundaryMatrix_mixed(NS,:) = (1 / ds);  % Neumann condition for mixed derivative
+
+        bm_SV = boundaryMatrix_mixed;
+
+        d1sM = MDerivativeVM(NS, ds, 0, 1);
+        d2sM = MSecondDerivativePlusCVM(NS, ds, 0, 1);
+        
+        d1SB = bm_S;
+        d2SB = bm_S2;
+        d1VB = bm_V1;
+        d2VB = zeros(NV,NS);
+        dSVB = bm_SV;
+
+        % Compute first and second derivatives in V
+    
+        d1vM = MDerivativeVM(NV, dv, 1, 1);
+        d2vM = MSecondDerivativePlusCVM(NV, dv, 1, 1);
+
+        IS = eye(NS);
+        IV = eye(NV);
+
+        %U is size NS,NV, the vectorised form is NS*NV,1
+        U_vec = U(:);
+
+        A1k = (r-q)*kron(IV,SMatrix*d1sM);
+        A2k = 0.5 * kron(VMatrix,S2Matrix*d2sM);
+        A3k = (kron((kappa * (theta * eye(NV) - VMatrix) - lambda * VMatrix)* d1vM,IS));
+        A4k = 0.5 * sigma^2 * kron(VMatrix * d2vM, IS);
+        A5k = rho * sigma * kron(VMatrix*d1vM, SMatrix*d1sM);
+
+        AK= A1k+A2k+A3k+A4k+A5k;
+
+        A1v = A1k * U_vec;
+        A2v = A2k * U_vec;
+        A3v = A3k * U_vec;
+        A4v = A4k * U_vec;
+        A5v = A5k * U_vec;
+
+        Av = A1v+A2v+A3v+A4v+A5v;
+
+        B1v = (r-q) * SMatrix * d1SB;
+        B2v = 0.5 * S2Matrix * d2SB * VMatrix;
+        B3v = ((kappa * (theta * eye(NV) - VMatrix) - lambda * VMatrix) * d1VB)';
+        B4v = 0.5 * sigma^2 * d2VB' * VMatrix;
+        B5v = rho * sigma * SMatrix * dSVB * VMatrix;
+
+        Bv = B1v(:)+B2v(:)+B3v(:)+B4v(:)+B5v(:);
+
+        lhs_matrix = ((1+(dt*r/2))*eye(NS*NV) - (dt/2)*AK);  
+        rhs_vector = ((1-(dt*r/2))*U_vec + (dt/2)*Av) + dt*Bv;
+
+        % Solve for U_vec at the next time step
+        % Set initial guess for GMRES
+        x0 = U_vec;  % The current solution vector
+
+        % Set GMRES parameters
+        restart = 3;  % Restart after 20 iterations (example value)
+        tol = 1e-3;  % Tolerance for convergence
+        max_iter = 10;  % Maximum number of iterations
+            
+        % Solve using GMRES
+        warning('off', 'all')
+        U_vec = restarted_gmres(lhs_matrix, rhs_vector, x0, restart, tol, max_iter);
+        warning('on', 'all')
+        
+        U = reshape(U_vec, [NS, NV]);
+        reconstructedU = X*Y';
+
+        diff = U-reconstructedU;
+
+        fprintf('Norm of diff %5.0f\n', norm(diff, 'fro'));
+
     end    
     U=X*Y';
 end
