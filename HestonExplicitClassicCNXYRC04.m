c1054e446eb2546ed7e1d2508bc1501dbacaabf4
Marco Abramo <marco.abramo@escaladetek.com>
2025-02-16 16:54:13 +0100

    let's see now

--- HestonExplicitClassicCNXYRC04.m
+++ HestonExplicitClassicCNXYRC04.m
@@ -70,6 +70,10 @@ function U = HestonExplicitClassicCNXYRC04(params,K,r,q,S,V,T, mode, iterations,
 	    Y(v)=1;
     end
     
+    %the following calculations are always the same at each cycle.
+    %therefore they have been taken outside the loop and cached as
+    %parameters
+
     SMatrix = diag(S);
     S2Matrix = diag(S.^2);
     VMatrix = diag(V);
@@ -93,7 +97,40 @@ function U = HestonExplicitClassicCNXYRC04(params,K,r,q,S,V,T, mode, iterations,
     A4KY = (0.5 * sigma^2)*VMatrix * d2vM;
     A5KX = SMatrix*d1sM;
     A5KY = rho * sigma * (VMatrix*d1vM);
+
+    % SMatrix = diag(S);
+    % S2Matrix = diag(S.^2);
+    % VMatrix = diag(V);
+
+    b1x = zeros(NS,1);
+    b1x(NS)=(1/ds);
+    b1y = ones(NV,1);
+
+    b2x = zeros(NS,1);
+    b2x(NS)=(1/ds);
+    b2y = ones(NV,1);
     
+    discountedPayoff = max((S - K * exp(-r * (Tmax - T(t)))), 0);
+
+    b3x = [discountedPayoff', S'];%In this case rank 2 because of the shape of the condition
+    b3y = zeros(NV,2); %2 columns because this is rank 2
+    b3y(1,1)=1;
+    b3y(NV,2)=1;
+   
+    b5x = zeros(NS,1);
+    b5x(NS)=(1/ds);
+    b5y = ones(NV,1);
+
+    b1x = (r-q) * SMatrix * b1x;
+    b2x = 0.5 * S2Matrix * b2x;
+    b2y = VMatrix'*b2y;
+    b3y = (kappa * (theta * eye(NV) - VMatrix) - lambda * VMatrix)' * b3y;
+    b4x = zeros(NS,1);
+    b4y = zeros(NV,1);
+    
+    b4y = 0.5 * sigma^2*VMatrix'*b4y;
+    b5x = rho * sigma * SMatrix * b5x;
+    b5y = VMatrix'*b5y;
 
     for t = 1:NT-1
         tol = 1e-5;  % Tolerance for convergence and compression
@@ -102,10 +139,12 @@ function U = HestonExplicitClassicCNXYRC04(params,K,r,q,S,V,T, mode, iterations,
         % x = X;
         % y = Y;
 
-        [A,B] = HestonModelOperator(NS, NV, ds, dv, S, V, r, q, kappa, theta, lambda, sigma, rho);
+        % [A,B] = HestonModelOperator(NS, NV, ds, dv, S, V, r, q, kappa, theta, lambda, sigma, rho);
+        [A,B] = HestonModelOperatorLean(A1KX,A1KY,A2KX,A2KY,A3KX,A3KY,A4KX,A4KY,A5KX,A5KY);
         [AX,AY] = LowRankMatVec(A,B,x,y);
-        [BX,BY] = HestonMatVecBoundaries(NS, NV, ds, dv, S, V, r, q, kappa, theta, lambda, sigma, rho, K, Tmax, t, T);
+        % [BX,BY] = HestonMatVecBoundaries(NS, NV, ds, dv, S, V, r, q, kappa, theta, lambda, sigma, rho, K, Tmax, t, T);
 
+        [BX,BY] = HestonMatVecBoundariesLean(b1x,b2x,b3x,b4x,b5x,b1y,b2y,b3y,b4y,b5y);
         %half Euler step
         FX = [(1-r*dt/2)*x,  (dt/2)*AX, dt*BX]; 
         FY = [           y,         AY,    BY];
