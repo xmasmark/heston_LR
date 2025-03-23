e8ec3316d02d4d53dac1b24368b5fb7bc5d5826a
Marco Abramo <marco.abramo@escaladetek.com>
2025-03-23 20:01:10 +0100

    More stress testing batches

---
+++ DEV_GMRES_Low_Rank_StressTesting.m
@@ -0,0 +1,69 @@
+% Illustration of pricing using uniform and non-uniform grids
+clc; clear;
+
+
+% HestonExplicitClassicCN_GMRS(params,K,r,q,S,V,T)
+% test = HestonExplicitClassicCN_GMRS(params,K,r,q,Sm,Vm,T);
+
+% Obtain the price by 2-D interpolation
+S0 = 101.52;
+V0 = 0.05412;
+%UniformPrice = interp2(V,S,U,V0,S0);
+
+%[price, error, time] = ALS_DataPoints(nS, nV, nT, S0, V0, cnIterations);
+nt = 1000;
+% % % % % % % [ClosedPrice10, price10, error10, time10] = ALS_DataPoints(29, 19, nt, S0, V0, 10);
+% % % % % % % [ClosedPrice20, price20, error20, time20] = ALS_DataPoints(29, 19, nt, S0, V0, 20);
+% % % % % % % [ClosedPrice30, price30, error30, time30] = ALS_DataPoints(29, 19, nt, S0, V0, 30);
+% % % % % % % [ClosedPrice40, price40, error40, time40] = ALS_DataPoints(29, 19, nt, S0, V0, 40);
+% % % % % % % [ClosedPrice50, price50, error50, time50] = ALS_DataPoints(29, 19, nt, S0, V0, 50);
+% % % % % % % [ClosedPrice100, price100, error100, time100] = ALS_DataPoints(29, 19, nt, S0, V0, 100);
+% % % % % % % % [ClosedPrice200, price200, error200, time200] = ALS_DataPoints(29, 19, 100, S0, V0, 200);
+% % % % % % % % [ClosedPrice300, price300, error300, time300] = ALS_DataPoints(29, 19, 100, S0, V0, 300);
+% % % % % % % 
+% % % % % % % %% Output the results
+% % % % % % % fprintf('-------------------------------------------------------------------------------------------------------------------------------------\n')
+% % % % % % % fprintf('Method                                          Price      Closed Price DollarError  Execution time       NS       NV    CNIterations\n')
+% % % % % % % fprintf('-------------------------------------------------------------------------------------------------------------------------------------\n')
+% % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price10, ClosedPrice10,error10, time10, 30, 20, 10)
+% % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price20, ClosedPrice20,error20, time20, 30, 20, 20)
+% % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price30, ClosedPrice20,error30, time30, 30, 20, 30)
+% % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price40, ClosedPrice20,error40, time40, 30, 20, 40)
+% % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price50, ClosedPrice20,error50, time50, 30, 20, 50)
+% % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price100, ClosedPrice100,error100, time100, 30, 20, 100)
+% % % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price200, ClosedPrice200,error200, time200, 30, 20, 200)
+% % % % % % % % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price300, ClosedPrice300,error300, time300, 30, 20, 300)
+% % % % % % % %fprintf('Dmitry is the BEST                                                         \n')
+% % % % % % % fprintf('-------------------------------------------------------------------------------------------------------------------------------------\n')
+% % % % % % % 
+
+%[price, error, time] = ALS_DataPoints(nS, nV, nT, S0, V0, cnIterations);
+nt = 200;
+cnIterations = 100;
+
+
+%[ClosedPrice, price, error, time] = GMRES_Low_Rank_DataPoints(nS, nV, nT, S0, V0, cnIterations)
+
+[ClosedPrice10, price10, error10, time10] = GMRES_Low_Rank_DataPoints(29, 19, nt, S0, V0, cnIterations);
+[ClosedPrice20, price20, error20, time20] = GMRES_Low_Rank_DataPoints(39, 29, nt, S0, V0, cnIterations);
+[ClosedPrice30, price30, error30, time30] = GMRES_Low_Rank_DataPoints(49, 39, nt, S0, V0, cnIterations);
+[ClosedPrice40, price40, error40, time40] = GMRES_Low_Rank_DataPoints(59, 49, nt, S0, V0, cnIterations);
+[ClosedPrice50, price50, error50, time50] = GMRES_Low_Rank_DataPoints(69, 59, nt, S0, V0, cnIterations);
+[ClosedPrice100, price100, error100, time100] = GMRES_Low_Rank_DataPoints(79, 69, nt, S0, V0, cnIterations);
+% [ClosedPrice200, price200, error200, time200] = ALS_DataPoints(29, 19, 100, S0, V0, 200);
+% [ClosedPrice300, price300, error300, time300] = ALS_DataPoints(29, 19, 100, S0, V0, 300);
+
+%% Output the results
+fprintf('-------------------------------------------------------------------------------------------------------------------------------------\n')
+fprintf('Method                                          Price      Closed Price DollarError  Execution time       NS       NV    CNIterations\n')
+fprintf('-------------------------------------------------------------------------------------------------------------------------------------\n')
+fprintf('Heston Classic GMRES Low Rank Lean         %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price10, ClosedPrice10,error10, time10, 30, 20, 100)
+fprintf('Heston Classic GMRES Low Rank Lean         %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price20, ClosedPrice20,error20, time20, 40, 30, 100)
+fprintf('Heston Classic GMRES Low Rank Lean         %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price30, ClosedPrice20,error30, time30, 50, 40, 100)
+fprintf('Heston Classic GMRES Low Rank Lean         %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price40, ClosedPrice20,error40, time40, 60, 50, 100)
+fprintf('Heston Classic GMRES Low Rank Lean         %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price50, ClosedPrice20,error50, time50, 70, 60, 100)
+fprintf('Heston Classic GMRES Low Rank Lean         %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price100, ClosedPrice100,error100, time100, 80, 70, 100)
+% fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price200, ClosedPrice200,error200, time200, 30, 20, 200)
+% fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %10.4f      %5.2f       %10.4f       %d       %d             %d\n',price300, ClosedPrice300,error300, time300, 30, 20, 300)
+%fprintf('Dmitry is the BEST                                                         \n')
+fprintf('-------------------------------------------------------------------------------------------------------------------------------------\n')
---
+++ GMRES_Low_Rank_DataPoints.m
@@ -0,0 +1,146 @@
+function [ClosedPrice, price, error, time] = GMRES_Low_Rank_DataPoints(nS, nV, nT, S0, V0, cnIterations)
+
+    % Illustration of pricing using uniform and non-uniform grids
+
+    
+    % restoredefaultpath
+    % rehash toolboxcache
+    
+    % Strike price, risk free rate, dividend yield, and maturity
+    K = 100;
+    r = 0.02;
+    q = 0.05;
+    Mat = 0.15;
+    
+    % Heston parameters
+    kappa =  1.5;
+    theta =  0.04;
+    sigma =  0.3;
+    rho   = -0.9;
+    v0    =  0.05;
+    lambda = 0;
+    params = [kappa theta sigma v0 rho lambda];
+    
+    % Minimum and maximum values for the Stock Price, Volatility, and Maturity
+    Smin = 0;  Smax = 2*K;
+    Vmin = 0;  Vmax = 0.5;
+    Tmin = 0;  Tmax = Mat;
+    
+    % Number of grid points for the stock, volatility, and maturity
+    % nS = 79;        % Stock price
+    % nV = 39;        % Volatility
+    
+    % nS = 29;        % Stock price -- 99 --29
+    % nV = 19;        % Volatility  -- 99 --19
+    % nT = 100;      % Maturity    -- 1000 
+    
+    % The maturity time increment and grid
+    dt = (Tmax-Tmin)/nT;
+    T = [0:nT].*dt;
+    
+    %% Pricing Using a Uniform Grid
+    % Increment for Stock Price and volatility
+    ds = (Smax-Smin)/nS;
+    dv = (Vmax-Vmin)/nV;
+    
+    % The stock price and volatility grids
+    S = [0:nS].*ds;
+    V = [0:nV].*dv;
+    
+    Sm = [1:(nS-1)].*ds;
+    Vm = [1:(nV-1)].*dv;
+    
+    
+    epsilon = 0.00001;
+    
+    
+    iterations = 10;
+    restart = 3;
+    
+    % 
+    % tic
+    % UvHEClassicCNGMRS = HestonExplicitClassicCNRC01(params,K,r,q,Sm,Vm,T,1,iterations,restart);
+    % timeElapsed = toc;
+    
+    
+    
+    % GMRES_Result = HestonExplicitClassicCN_GMRS(params,K,r,q,S,V,T);
+    
+    %P = HestonExplicitClassicCN_GMRS(params,K,r,q,S,V,T);
+    % tic
+    % UvHEClassicCNXYdev = HestonExplicitClassicCNXYRC04(params,K,r,q,Sm,Vm,T,2, iterations, restart);
+    % timeElapsedXY = toc;
+    
+    tic
+
+    %UvHEClassicCNXYLeanDev = HestonExplicitClassicCNALSDev10(params,K,r,q,Sm,Vm,T,2,iterations, restart, cnIterations);
+    UvHEClassicCNXYLeanDev = HestonExplicitClassicCNXYRC04(params,K,r,q,Sm,Vm,T,2, iterations, restart);
+    timeElapsedLean = toc;
+    
+    %Suliko = HestonExplicitClassicCNXYCOMP(params,K,r,q,S,V,T);
+    
+    % HestonExplicitClassicCN_GMRS(params,K,r,q,S,V,T)
+    % test = HestonExplicitClassicCN_GMRS(params,K,r,q,Sm,Vm,T);
+    
+    % Obtain the price by 2-D interpolation
+    % S0 = 101.52;
+    % V0 = 0.05412;
+    %UniformPrice = interp2(V,S,U,V0,S0);
+    
+    % UvHEClassicCNXYdevPrice = interp2(Vm,Sm,UvHEClassicCNXYdev,V0,S0);
+    UvHEClassicCNXYLeanDevPrice = interp2(Vm,Sm,UvHEClassicCNXYLeanDev,V0,S0);
+    
+    %% Pricing Using a Non-Uniform Grid
+    % The stock price grid
+    c = K/5;
+    dz = 1/nS*(asinh((Smax-K)/c) - asinh(-K/c));
+    for i=1:nS+1;
+	    z(i) = asinh(-K/c) + (i-1)*dz;
+	    S(i) = K + c*sinh(z(i));
+    end
+    
+    % The volatility grid
+    d = Vmax/500;
+    dn = asinh(Vmax/d)/nV;
+    for j=1:nV+1
+	    n(j) = (j-1)*dn;
+	    V(j) = d*sinh(n(j));
+    end
+    
+    % Solve the PDE
+    U = HestonExplicitPDENonUniformGrid(params,K,r,q,S,V,T);
+    
+    % Obtain the price by 2-D interpolation
+    NonUniformPrice = interp2(V,S,U,V0,S0);
+    
+    
+    %% Closed form Price and errors
+    trap = 1;
+    PutCall = 'C';
+    [x w] = GenerateGaussLaguerre(32);
+    ClosedPrice = HestonPriceGaussLaguerre(PutCall,S0,K,Mat,r,q,kappa,theta,sigma,lambda,V0,rho,trap,x,w);
+    
+    % UvHEClassicCNXYdevError = UvHEClassicCNXYdevPrice - ClosedPrice;
+    UvHEClassicCNXYLeanDevError = UvHEClassicCNXYLeanDevPrice - ClosedPrice;
+    
+    
+    % %% Output the results
+    % %clc;
+    % fprintf('Stock price grid size  %5.0f\n', nS+1)
+    % fprintf('Volatility grid size   %5.0f\n', nV+1)
+    % fprintf('Number of time steps   %5.0f\n', nT)
+    % fprintf('-----------------------------------------------------------------------------------\n')
+    % fprintf('Method                                          Price    DollarError Execution time\n')
+    % fprintf('-----------------------------------------------------------------------------------\n')
+    % fprintf('Closed Form                                %10.4f              \n', ClosedPrice)
+    % fprintf('Heston Classic CN GMRS Low Rank Super Dima %10.4f        %5.2f       %10.4f\n', UvHEClassicCNXYdevPrice,UvHEClassicCNXYdevError, timeElapsedXY)
+    % %fprintf('Heston Classic CN GMRS Low Rank Lean       %10.4f        %5.2f       %10.4f\n\n', UvHEClassicCNXYLeanDevPrice,UvHEClassicCNXYLeanDevError, timeElapsedLean)
+    % fprintf('Heston Classic CN ALS  Low Rank Lean       %10.4f        %5.2f       %10.4f\n\n', UvHEClassicCNXYLeanDevPrice,UvHEClassicCNXYLeanDevError, timeElapsedLean)
+    % %fprintf('Dmitry is the BEST                                                         \n')
+    % fprintf('-----------------------------------------------------------------------------------\n')
+    
+    price = UvHEClassicCNXYLeanDevPrice;
+    error = UvHEClassicCNXYLeanDevError;
+    time = timeElapsedLean;
+
+end
